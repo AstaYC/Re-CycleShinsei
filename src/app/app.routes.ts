a00c56356838c64879f7c9d1252e1fa1e0eb1b79
AstaYC PS <baihmohamedamine@gmail.com>
2025-02-07 19:14:02 +0100

    i added the edit profil feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,18 @@
   import {LoginComponent} from "./auth/login/login.component";
   import {ProfileComponent} from "./profile/profile.component";
   import {EditProfileComponent} from "./profile/edit-profile/edit-profile.component";
+  import {SubmitRequestComponent} from "./collection/submit-request/submit-request.component";
+  import {MyRequestsComponent} from "./collection/my-requests/my-requests.component";
+  import {CollectorDashboardComponent} from "./collection/collector-dashboard/collector-dashboard.component";
 
   export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'profile', component: ProfileComponent },
-    { path: 'profile/edit', component: EditProfileComponent }, // Edit profile route
+    { path: 'profile/edit', component: EditProfileComponent },
+    { path: 'collection/submit', component: SubmitRequestComponent },
+    { path: 'collection/my-requests', component: MyRequestsComponent },
+    { path: 'collection/dashboard', component: CollectorDashboardComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
   ];
 
---
+++ src/app/collection/submit-request/submit-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './submit-request.component.html',
+  styleUrl: './submit-request.component.css'
+})
+export class SubmitRequestComponent {
+
+}
