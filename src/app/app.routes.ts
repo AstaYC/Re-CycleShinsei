36505c0fa29b768ba5990ce8343db3cb1de6f98b
AstaYC PS <baihmohamedamine@gmail.com>
2025-02-10 04:41:24 +0100

    fix some issue in the assets and app.module*

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,37 @@
-  import { NgModule } from '@angular/core';
-  import { RouterModule, Routes } from '@angular/router';
-  import { RegisterComponent } from './auth/register/register.component';
-  import {LoginComponent} from "./auth/login/login.component";
-  import {ProfileComponent} from "./profile/profile.component";
-  import {EditProfileComponent} from "./profile/edit-profile/edit-profile.component";
-  import {SubmitRequestComponent} from "./collection/submit-request/submit-request.component";
-  import {MyRequestsComponent} from "./collection/my-requests/my-requests.component";
-  import {CollectorDashboardComponent} from "./collection/collector-dashboard/collector-dashboard.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import {NoAuthGuard} from "./core/guards/no-auth.guard";
 
-  export const routes: Routes = [
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'profile/edit', component: EditProfileComponent },
-    { path: 'collection/submit', component: SubmitRequestComponent },
-    { path: 'collection/my-requests', component: MyRequestsComponent },
-    { path: 'collection/dashboard', component: CollectorDashboardComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-  ];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'auth',
+    canActivate: [NoAuthGuard],
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'profile',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: 'points',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./features/points/points.module').then(m => m.PointsModule)
+  }
+];
 
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule {}
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
